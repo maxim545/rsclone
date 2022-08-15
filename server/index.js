f6c9a9289f5b2156ff5f322c9cef20dbe84d94ed
maxim545 <maxximc95@gmail.com>
2022-08-15 19:34:16 +0300

    feat: connect mongoDB to the project

--- server/index.js
+++ server/index.js
@@ -2,7 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser'
 
-
+dotenv.config();
+mongoose.connect(
+  process.env.MONGODB_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+).then(() => { console.log('DB is started') })
 
 const app = express()
 const port = 3000;

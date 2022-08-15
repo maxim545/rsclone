564d7ade3f4bfe6b694d36c8275e5cbf6d1c3b05
maxim545 <maxximc95@gmail.com>
2022-08-15 17:29:51 +0300

    feat: add products data

---
+++ server/data.js
@@ -0,0 +1,605 @@
+module.exports = {
+    products: [
+        {
+            id: '111111111111111111111111',
+            name: 'Blue adidas jeans 2010',
+            year: 2010,
+            color: 'blue',
+            category: 'jeans',
+            price: 255,
+            brand: 'adidas',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "S",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '222222222222222222222222',
+            name: 'Grey sneakers 2016',
+            year: 2016,
+            color: 'grey',
+            category: 'sneakers',
+            price: 125,
+            brand: 'nike',
+            image: '',
+            variant: [
+                {
+                    size: "42",
+                    stock: 5
+                },
+                {
+                    size: "43",
+                    stock: 12
+                }
+            ]
+
+        },
+        {
+            id: '3',
+            name: 'Blue jeans 2020',
+            year: 2020,
+            color: 'blue',
+            category: 'jeans',
+            price: 154,
+            brand: 'adidas',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '4',
+            name: 'Pink body shirt 2020',
+            year: 2020,
+            color: 'pink',
+            category: 'body shirt',
+            price: 55,
+            brand: 'puma',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '5',
+            name: 'white sneakers 2022',
+            year: '2022',
+            color: 'white',
+            category: 'sneakers',
+            price: '103',
+            brand: 'nike',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '6',
+            name: 'Blue coat 2011',
+            year: '2011',
+            color: 'blue',
+            category: 'coat',
+            price: '299',
+            brand: 'columbia',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '7',
+            name: 'Blue T-shirt 2021',
+            year: '2021',
+            color: 'blue',
+            category: 'T-shirt',
+            price: '147',
+            brand: 'armani',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '8',
+            name: 'Cap white 2010',
+            year: '2010',
+            color: 'white',
+            category: 'Cap',
+            price: '15',
+            brand: 'puma',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '9',
+            name: 'Blue T-shirt 2010',
+            year: '2010',
+            color: 'blue',
+            category: 'T-shirt',
+            price: '183',
+            brand: 'nike',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '10',
+            name: 'Red coat 2022',
+            year: '2022',
+            color: 'red',
+            category: 'coat',
+            price: '285',
+            brand: 'nike',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '11',
+            name: 'Green T-shirt 2022',
+            year: '2022',
+            color: 'green',
+            category: 'T-shirt',
+            price: '145',
+            brand: 'adidas',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '12',
+            name: 'Yellow T-shirt 2014',
+            year: '2014',
+            color: 'yellow',
+            category: 'T-shirt',
+            price: '24',
+            brand: 'columbia',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '13',
+            name: 'Black boots 2012',
+            year: '2012',
+            color: 'black',
+            category: 'boots',
+            price: '165',
+            brand: 'nike',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '14',
+            name: 'Purple jacket 2012',
+            year: '2012',
+            color: 'purple',
+            category: 'jacket',
+            price: '300',
+            brand: 'adidas',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '15',
+            name: 'Red jacket 2018',
+            year: '2018',
+            color: 'red',
+            category: 'jacket',
+            price: '38',
+            brand: 'nike',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '16',
+            name: 'Yellow jacket 2010',
+            year: '2010',
+            color: 'yellow',
+            category: 'jacket',
+            price: '259',
+            brand: 'armani',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '17',
+            name: 'Purple sneakers 2019',
+            year: '2019',
+            color: 'pink',
+            category: 'sneakers',
+            price: '45',
+            brand: 'puma',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '18',
+            name: 'Red jeans 2022',
+            year: '2022',
+            color: 'red',
+            category: 'jeans',
+            price: '241',
+            brand: 'armani',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '19',
+            name: 'Purple 2014',
+            year: '2014',
+            color: 'purple',
+            category: 'jeans',
+            price: '175',
+            brand: 'nike',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '20',
+            name: 'Yellow body shirt 2011',
+            year: '2011',
+            color: 'yellow',
+            category: 'shirt',
+            price: '199',
+            brand: 'nike',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '21',
+            name: 'Blue boots 2022',
+            year: '2022',
+            color: 'blue',
+            category: 'boots',
+            price: '99',
+            brand: 'puma',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '22',
+            name: 'Black cap 2022',
+            year: '2022',
+            color: 'black',
+            category: 'cap',
+            price: '49',
+            brand: 'nike',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '23',
+            name: 'Orange cap 2017',
+            year: '2017',
+            color: 'orange',
+            category: 'cap',
+            price: '12',
+            brand: 'armani',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '24',
+            name: 'Geen cap 2018',
+            year: '2018',
+            color: 'green',
+            category: 'cap',
+            price: '7',
+            brand: 'armani',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '25',
+            name: 'Blue cap 2021',
+            year: '2021',
+            color: 'blue',
+            category: 'cap',
+            price: '47',
+            brand: 'armani',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '26',
+            name: 'Red cap 2010',
+            year: '2010',
+            color: 'red',
+            category: 'cap',
+            price: '12',
+            brand: 'armani',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '27',
+            name: 'Purple sneakers 2014',
+            year: '2014',
+            color: 'purple',
+            category: 'sneakers',
+            price: '299',
+            brand: 'adidas',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '28',
+            name: 'Yellow sneakers 2017',
+            year: '2017',
+            color: 'yellow',
+            category: 'sneakers',
+            price: '122',
+            brand: 'nike',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '29',
+            name: 'Green coat 2022',
+            year: '2022',
+            color: 'green',
+            category: 'coat',
+            price: '257',
+            brand: 'puma',
+            image: '',
+            variant: [
+                {
+                    size: "L",
+                    stock: 5
+                },
+                {
+                    size: "M",
+                    stock: 2
+                }
+            ]
+        },
+        {
+            id: '30',
+            name: 'Yellow boots 2019',
+            year: '2019',
+            color: 'yellow',
+            category: 'boots',
+            price: '200',
+            brand: 'puma',
+            image: '',
+            variant: [
+                {
+                    size: "42",
+                    stock: 5
+                },
+                {
+                    size: "43",
+                    stock: 2
+                },
+            ]
+        },
+    ],
+};

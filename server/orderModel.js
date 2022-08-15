4dc8c6c27e0aebcbd3f22b78ac975ff505265dfc
maxim545 <maxximc95@gmail.com>
2022-08-16 00:33:07 +0300

    feat: add model of order on server

---
+++ server/orderModel.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const orderSchema = new mongoose.Schema(
+    {
+        orderItems: [
+            {
+                name: { type: String, required: true },
+            },
+        ],
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+const Order = mongoose.model('Order', orderSchema);
+export default Order;

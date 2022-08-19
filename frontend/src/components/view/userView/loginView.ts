2bece2fd02e68bc910c1a491b741a54c6ced6ef2
maxim545 <maxximc95@gmail.com>
2022-08-20 02:00:03 +0300

    feat: add design for login page

--- frontend/src/components/view/userView/loginView.ts
+++ frontend/src/components/view/userView/loginView.ts
@@ -16,28 +16,33 @@ class LoginView extends Element {
     }
 
     create() {
-        const loginEl = this.createEl('div', '', 'login', null);
-        this.createEl('h2', 'Please enter your Name and password', 'login__title', loginEl);
+        const container = this.createEl('div', '', 'container_main', null);
+        const loginEl = this.createEl('div', '', 'login', container);
+
+        this.createEl('h2', 'Sign in', 'login__title', loginEl);
+        this.createEl('p', 'Sign in to your account using email and password provided during registration.', 'login__subtitle', loginEl);
         const inpustList = this.createEl('div', '', 'login__list', loginEl);
 
+
         const emailEl = this.createEl('div', '', 'login__item', inpustList);
-        this.createEl('p', 'email', 'login__title', emailEl);
-        const inputEmail = this.createEl('input', '', `login__email`, emailEl) as HTMLInputElement;
+        this.createEl('p', 'Email', 'login__input-title', emailEl);
+        const inputEmail = this.createEl('input', '', `form-control login__input`, emailEl) as HTMLInputElement;
         inputEmail.type = 'email';
+        inputEmail.placeholder = 'Your working email'
 
         const passwordEl = this.createEl('div', '', 'login__item', inpustList);
-        this.createEl('p', 'password', 'login__title', passwordEl);
-        const inputPassword = this.createEl('input', '', `login__password`, passwordEl) as HTMLInputElement;
+        this.createEl('p', 'Password', 'login__input-title', passwordEl);
+        const inputPassword = this.createEl('input', '', `form-control login__input`, passwordEl) as HTMLInputElement;
         inputPassword.type = 'password';
 
-        const submit = this.createEl('button', 'submit', 'login__btn', loginEl);
-        this.createEl('a', 'Register new account', 'login__register', loginEl, `#/register`) as HTMLAnchorElement;
+        const submit = this.createEl('button', 'Sign in', 'btn btn-primary login__btn', loginEl);
+        this.createEl('p', `Don't have an account? <a class="login__link" href="#/register">Sign up</a>`, 'login__info', loginEl) as HTMLAnchorElement;
         submit.addEventListener('click', () => {
             this.controller.loginUser(inputEmail.value, inputPassword.value)
                 .then(() => { this.updateView.updateHeader() })
         });
 
-        return loginEl;
+        return container;
     }
 
 }

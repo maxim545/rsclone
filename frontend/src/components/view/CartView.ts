d896b9fe2a71faa9390659e8d6882c0ae5872c03
maxim545 <maxximc95@gmail.com>
2022-08-21 00:29:02 +0300

    feat: add design for items on cart page

--- frontend/src/components/view/CartView.ts
+++ frontend/src/components/view/CartView.ts
@@ -7,56 +7,64 @@ import UpdateView from "../Update";
 
 class CartView extends Element {
 
-    private api: Api
+    private api: Api;
 
-    private controller: Controller
+    private controller: Controller;
 
-    private updateView: UpdateView
+    private updateView: UpdateView;
+
+    /*     private userData: IUserData | null; */
 
     constructor() {
         super();
         this.api = new Api();
         this.controller = new Controller();
         this.updateView = new UpdateView();
+        /*         this.userData = <IUserData>JSON.parse(localStorage.getItem('userData') || 'null'); */
     }
 
     create() {
-        const cartEl = this.createEl('div', '', 'container', null);
+        const container = this.createEl('div', '', 'container_main cart', null);
+        const cartEl = this.createEl('div', '', 'cart__content', container);
         const cartsId = <string[]>JSON.parse(localStorage.getItem('cartData') || 'null');
         const userData = <IUserData>JSON.parse(localStorage.getItem('userData') || 'null');
         const cartsData: IOrderData = {
             orderItems: [],
         };
         if (cartsId && cartsId.length) {
+            this.createSideBar(container);
+            this.createEl('h2', 'Checkout', 'cart__title', cartEl);
             (async () => {
-                this.createEl('h2', 'Fill in all the data to place an order', 'account__title', cartEl);
-                const inpustList = this.createEl('div', '', 'account__list', cartEl);
-                const inputs = ['name:text', 'email:email', 'surname:text', 'thirdname:text', 'phone:text', 'adress:text'];
-                if (!userData) { inputs.push('password:password', 'repeatPassword:password') }
-                const unputsValues: IUserData = {}
-                inputs.forEach(item => {
-                    const [name, type] = item.split(':');
-                    const inputContainer = this.createEl('div', '', 'login__item', inpustList);
-                    this.createEl('p', `Your ${name}`, 'login__title', inputContainer);
-                    const input = this.createEl('input', '', name, inputContainer) as HTMLInputElement;
-                    input.type = type;
-                    input.addEventListener('change', () => { unputsValues[name as keyof typeof unputsValues] = input.value })
-                    if (userData && userData[name as keyof typeof userData] !== undefined) {
-                        const currentValue = userData[name as keyof typeof userData] as string;
-                        input.value = currentValue;
-                        unputsValues[name as keyof typeof unputsValues] = input.value;
-                    }
-                })
-                // Functional showing carts of pruduct
+                const [inpustList, unputsValues] = this.createInputs() as [HTMLElement, IUserData];
+                const cartListEl = this.createEl('div', '', 'cart__list', cartEl);
+                this.createEl('h3', '1. Item Review', 'cart__list-title', cartListEl);
+                const cartItemsEl = this.createEl('div', '', 'cart__items', cartListEl);
+                let totalPrice = 0;
                 for await (const item of cartsId) {
                     const product = <IProduct>await this.api.getProduct(item);
                     cartsData.status = 'processing';
                     cartsData.orderItems.push(product);
                     if (product) {
-                        this.createEl('div', product.name, 'item__name', cartEl);
-                        this.createEl('div', product.year, 'item__year', cartEl);
-                        this.createEl('div', product.color, 'item__year', cartEl);
-                        const removeBtn = this.createEl('button', 'Remove', 'item__remove-btn', cartEl);
+                        const cartItemEl = this.createEl('div', `<img src="${product.image}" class="cart__item-img" alt="image">`, 'cart__item', cartItemsEl);
+                        const itemInfo = this.createEl('div', '', 'cart__item-info', cartItemEl);
+                        this.createEl('div', product.name, 'cart__item-name', itemInfo);
+                        this.createEl('div', `<span class="cart__item-name-key">Year:</span> ${product.color}`, 'cart__item-name-value', itemInfo);
+                        this.createEl('div', `<span class="cart__item-name-key">Size:</span> ${product.year}`, 'cart__item-name-value', itemInfo);
+                        /* const itemAmount =  */this.createEl('div', 'Amount', 'cart__item-amount', cartItemEl);
+
+
+                        const sumPrice = product.price;
+                        const discPrice = Number(sumPrice) - Number(sumPrice) * Number(product.discount) / 100;
+                        totalPrice += discPrice;
+                        if (Number(product.discount)) {
+                            this.createEl('div', `$${discPrice}<span class="cart__item-price cart__item-price_grey">$${sumPrice}</span>`, 'cart__item-price cart__item-price_orange', cartItemEl);
+                        } else {
+                            this.createEl('div', `$${product.price}`, 'cart__item-price', cartItemEl);
+                        }
+
+
+
+                        const removeBtn = this.createEl('button', 'Delete', 'btn btn-primary cart__item-btn', cartItemEl);
                         removeBtn.addEventListener('click', () => {
                             this.controller.removeFromCart(cartsId, product._id)
                             const main = document.querySelector('.main') as HTMLElement;
@@ -65,15 +73,48 @@ class CartView extends Element {
                         });
                     }
                 }
+
+                this.createEl('div', `Subtotal: $${totalPrice}`, 'cart__subtotal', cartListEl);
+
+                cartEl.append(inpustList)
                 if (cartsData.orderItems.length) {
-                    this.createPurchases(cartEl, cartsData, userData, unputsValues)
+                    this.addOrder(cartEl, cartsData, userData, unputsValues)
                 }
             })().catch(err => { console.error(err) });
         }
-        return cartEl;
+        return container;
     }
 
-    createPurchases(cartEl: HTMLElement, cartsData: IOrderData, userData: IUserData, unputsValues: IUserData) {
+    createInputs() {
+        const userData = <IUserData>JSON.parse(localStorage.getItem('userData') || 'null');
+        const shippingList = this.createEl('div', '', 'shipping__list', null);
+        this.createEl('h2', '2. Shipping & Billing Address', 'cart__list-title', shippingList);
+        const inpustListEl = this.createEl('div', '', 'account__inputs-list', shippingList);
+        const inputs = ['name:text', 'email:email', 'surname:text', 'thirdname:text', 'phone:text', 'adress:text'];
+        if (!userData) { inputs.push('password:password', 'repeatPassword:password') }
+        const unputsValues: IUserData = {}
+        inputs.forEach(item => {
+            const [name, type] = item.split(':');
+            const inputContainer = this.createEl('div', '', 'account__inputs-item', inpustListEl);
+            this.createEl('p', `Your ${name}`, 'account__inputs-title', inputContainer);
+            const input = this.createEl('input', '', 'form-control account__input', inputContainer) as HTMLInputElement;
+            input.type = type;
+            input.addEventListener('change', () => { unputsValues[name as keyof typeof unputsValues] = input.value })
+            if (userData && userData[name as keyof typeof userData] !== undefined) {
+                const currentValue = userData[name as keyof typeof userData] as string;
+                input.value = currentValue;
+                unputsValues[name as keyof typeof unputsValues] = input.value;
+            }
+        })
+        return [shippingList, unputsValues]
+    }
+
+    createSideBar(container: HTMLElement) {
+        const sidebar = this.createEl('div', '<h2 class="cart__sidebar-title">Order totals</h2>', 'cart__sidebar', container);
+        return sidebar
+    }
+
+    addOrder(cartEl: HTMLElement, cartsData: IOrderData, userData: IUserData, unputsValues: IUserData) {
         const purchases = this.createEl('div', 'Your purchases list with prices', 'purchases', cartEl);
         const orderBtn = this.createEl('button', 'create order', 'purchases__btn', purchases);
         orderBtn.addEventListener('click', () => {

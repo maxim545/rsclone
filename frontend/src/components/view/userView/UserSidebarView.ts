2e2b87a328d2006bc62a20c0dfd9ba5a52337390
maxim545 <maxximc95@gmail.com>
2022-08-21 17:33:21 +0300

    refactor: resolve conflicts

--- frontend/src/components/view/userView/UserSidebarView.ts
+++ frontend/src/components/view/userView/UserSidebarView.ts
@@ -19,6 +19,7 @@ class UserSidebarView extends Element {
 
     create(userData: IUserData) {
         const sidebar = this.createEl('div', '', 'sidebar', null);
+        const wrapper = this.createEl('div', '', 'sidebar__wrapper', sidebar);
         (async () => {
             const links = ['Cart:#/cart', 'My profile:#/account', 'Favorites:#/favorites', 'My orders:#/purchases', 'Sign out:/'];
             const [currentUser] = await this.api.loginUser({
@@ -30,10 +31,10 @@ class UserSidebarView extends Element {
             }
             const currentHash = window.location.hash;
             if (userData.name && userData.surname && userData.email) {
-                this.createEl('h2', `${userData.name} ${userData.surname}`, 'sidebar__user-name', sidebar);
-                this.createEl('p', userData.email, 'sidebar__user-email', sidebar);
+                this.createEl('h2', `${userData.name} ${userData.surname}`, 'sidebar__user-name', wrapper);
+                this.createEl('p', userData.email, 'sidebar__user-email', wrapper);
             }
-            const linksEl = this.createEl('div', '', 'sidebar__links', sidebar);
+            const linksEl = this.createEl('div', '', 'sidebar__links', wrapper);
             links.forEach(link => {
                 const [name, hash] = link.split(':');
                 const linkEl = this.createEl('a', `${name}`, `sidebar__link`, linksEl, hash);

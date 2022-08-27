6a00cd25e0a6961a723734641231505c82f9b940
maxim545 <maxximc95@gmail.com>
2022-08-27 18:33:00 +0300

    refactor: resolve conflicts

--- frontend/src/components/view/userView/WishlistView.ts
+++ frontend/src/components/view/userView/WishlistView.ts
@@ -5,6 +5,7 @@ import Controller from "../../Controller";
 import UpdateView from "../../Update";
 import UserSidebarView from "./UserSidebarView";
 import Api from "../../api";
+import AlertsView from "../AlertsView";
 
 class AccountView extends Element {
 
@@ -16,39 +17,67 @@ class AccountView extends Element {
 
     private api: Api;
 
+    private alertView: AlertsView;
+
     constructor() {
         super()
         this.controller = new Controller();
         this.updateView = new UpdateView();
         this.sidebarView = new UserSidebarView();
         this.api = new Api();
+        this.alertView = new AlertsView();
     }
 
     create() {
         const container = this.createEl('div', '', 'container_main wishlist', null);
         const userData = <IUserData>JSON.parse(localStorage.getItem('userData') || 'null');
         if (!userData) {
+            const title = 'You are not registred';
             const enterLink = `<a class="acoount__link" href="#/login">enter</a>`
             const registerLink = `<a class="acoount__link" href="#/register">register</a>`
-            this.createEl('div', `Please ${enterLink} in your account or ${registerLink}`, 'account__warning', container);
+            const text = `Please ${enterLink} in your account or ${registerLink}`
+            container.append(this.alertView.createStaticAlert(title, text))
         }
         else {
             container.append(this.sidebarView.create(userData))
-            const wishlistEl = this.createEl('h2', '', 'wishlist__wrapper', container);
-            this.createEl('div', 'Wish list', 'wishlist__title', wishlistEl);
+            const wrapper = this.createEl('div', '', 'wishlist__wrapper', container);
+            /* const wishlistTitle =  */this.createEl('h2', 'Wishlist', 'wishlist__title', wrapper);
+            const wishlistItems = this.createEl('div', '', 'wishlist__list', wrapper);
             (async () => {
                 const wishList = await this.api.getAllWishItems(userData) as IWishListData[];
                 for (const item of wishList) {
                     const product = await this.api.getProduct(item.productId) as IProduct;
-                    const itemEl = this.createEl('div', '', 'item', wishlistEl);
-                    this.createEl('a', product.name, 'item__name', itemEl, `/#/p/${product._id}`);
-                    const image = `<img src="${product.image}" class="img-thumbnail" alt="image">`
-                    this.createEl('div', image, 'image-container', itemEl)
-                    this.createEl('div', product.year, 'item__year', itemEl);
-                    const removeBtn = this.createEl('button', 'remove', 'item__btn', itemEl);
-                    removeBtn.addEventListener('click', () => {
+                    const itemEl = this.createEl('div', '', 'wishlit__item', wishlistItems);
+                    itemEl.dataset.num = item._id;
+                    const image = `<img src="${product.image}" class="wishlit__item-img" alt="image">`
+                    const imageEl = this.createEl('div', image, 'wishlit__item-img-wrapper', itemEl);
+                    const itemInfoEl = this.createEl('div', '', 'wishlit__item-info', itemEl);
+                    const addBtn = this.createEl('a', '<i class="bi bi-heart-fill"></i>', 'wishlit__item-btn', imageEl);
+                    addBtn.dataset.id = item._id;
+                    this.createEl('a', product.name, 'wishlit__item-name', itemInfoEl, `/#/p/${product._id}`);
+
+
+                    const discPrice = (Number(product.price) - Number(product.price) * Number(product.discount) / 100);
+                    const withoutDisc = Number(product.price);
+                    if (Number(product.discount)) {
+                        const discountEl = this.createEl('div', `-${product.discount}%`, 'wishlit__item-discount', imageEl);
+                        const discWrapperEl = this.createEl('div', '', 'wishlit__item-discount-wrapper', itemInfoEl);
+                        this.createEl('div', `$${discPrice.toFixed(1)}`, 'wishlit__item-price wishlit__item-price_orange', discWrapperEl);
+                        this.createEl('div', `$${withoutDisc.toFixed(1)}`, 'wishlit__item-price wishlit__item-price_gray', discWrapperEl);
+                    } else {
+                        this.createEl('div', `$${discPrice.toFixed(1)}`, 'wishlit__item-price wishlit__item-price_bold', itemInfoEl);
+                    }
+
+
+                    addBtn.addEventListener('click', (e) => {
                         this.api.removeWishItem(userData, (item._id) as string).then(() => {
                             this.updateView.updateWishlistNum();
+                            e.preventDefault();
+                            const currentId = addBtn.dataset.id;
+                            const currentEl = document.querySelector(`div[data-num="${currentId}"]`);
+                            if (currentEl) {
+                                wishlistItems.removeChild(currentEl);
+                            }
                         });
                     })
                 }

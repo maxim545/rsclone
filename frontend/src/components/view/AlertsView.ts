2328d27106f846b9199f344a6a29042bb8da727b
maxim545 <maxximc95@gmail.com>
2022-08-28 11:59:06 +0300

    feat: add alerts for other pages

--- frontend/src/components/view/AlertsView.ts
+++ frontend/src/components/view/AlertsView.ts
@@ -3,9 +3,9 @@ import Element from "../common/Element";
 class PurchaseView extends Element {
 
     createEmptyAlert(page: string) {
-        const title = `You haven't added anything to your ${page}`;
+        const title = `You haven't added anything to your ${page}.`;
         const shopLink = `<a class="acoount__link" href="/#">main page</a>`
-        const text = `Go to the ${shopLink} and select the product you are interested in`
+        const text = `Go to the ${shopLink} and select the product you are interested in.`
         const container = this.createEl('div', '', `alert alert-warning`, null);
         this.createEl('h4', title, 'alert-heading', container);
         this.createEl('p', text, 'alert-text', container);
@@ -13,10 +13,10 @@ class PurchaseView extends Element {
     }
 
     createNotLoginAlert() {
-        const title = 'You are not registred';
-        const enterLink = `<a class="acoount__link" href="#/login">enter</a>`
+        const title = 'You are not registred.';
+        const enterLink = `<a class="acoount__link" href="#/login">sign in</a>`
         const registerLink = `<a class="acoount__link" href="#/register">register</a>`
-        const text = `Please ${enterLink} in your account or ${registerLink}`
+        const text = `Please ${enterLink} in your account or ${registerLink} new account.`
         const container = this.createEl('div', '', `alert alert-warning`, null);
         this.createEl('h4', title, 'alert-heading', container);
         this.createEl('p', text, 'alert-text', container);
@@ -24,8 +24,8 @@ class PurchaseView extends Element {
     }
 
     createNotAdminAlert() {
-        const title = 'You are not admin';
-        const text = `You must have administrator rights to access this page`
+        const title = 'You are not admin.';
+        const text = `You must have administrator rights to access this page.`
         const container = this.createEl('div', '', `alert alert-danger`, null);
         this.createEl('h4', title, 'alert-heading', container);
         this.createEl('p', text, 'alert-text', container);
--- frontend/src/components/view/adminView/adminProductsView.ts
+++ frontend/src/components/view/adminView/adminProductsView.ts
@@ -4,70 +4,77 @@ import Controller from "../../Controller";
 import { IProduct, IUserData } from "../../types";
 import UpdateView from "../../Update";
 import UserSidebarView from "../userView/UserSidebarView";
+import AlertsView from "../AlertsView";
 
 class AdminProductsView extends Element {
 
-    private api: Api
+  private api: Api
 
-    private controller: Controller
+  private controller: Controller
 
-    private updateView: UpdateView
+  private updateView: UpdateView
 
-    private sidebarView: UserSidebarView
+  private sidebarView: UserSidebarView
 
-    constructor() {
-        super();
-        this.api = new Api();
-        this.controller = new Controller();
-        this.updateView = new UpdateView();
-        this.sidebarView = new UserSidebarView();
-    }
+  private alertView: AlertsView;
 
-    create() {
-        const container = this.createEl(`div`, ``, `container_main account admin-products-wrapper`, null);
-        const userData = <IUserData>JSON.parse(localStorage.getItem(`userData`) || `null`);
-        if (!userData) {
-            const enterLink = `<a class="acoount__link" href="#/login">enter</a>`
-            const registerLink = `<a class="acoount__link" href="#/register">register</a>`
-            this.createEl(`div`, `Please ${enterLink} in your account or ${registerLink}`, `account__warning`, container);
-        }
-        else {
-            container.append(this.sidebarView.create(userData));
-            const productsSection = this.createEl(`section`, ``, `admin-products`, container);
-            const productsEl = this.createEl(`div`, `Admin products`, `admin-products`, null);
-            this.createEl(`a`, `create new product`, `admin-products__name`, productsEl, `#/adminpanel/createproduct`);
-            (async () => {
-                const products = await this.api.getAllProduct();
-                console.dir(products);
-                productsSection.innerHTML = `
+  constructor() {
+    super();
+    this.api = new Api();
+    this.controller = new Controller();
+    this.updateView = new UpdateView();
+    this.sidebarView = new UserSidebarView();
+    this.alertView = new AlertsView();
+  }
+
+  create() {
+    const container = this.createEl(`div`, ``, `container_main account admin-products-wrapper`, null);
+    const userData = <IUserData>JSON.parse(localStorage.getItem(`userData`) || `null`);
+    (async () => {
+      const [currentUser] = await this.api.loginUser({
+        email: userData.email,
+        password: userData.password
+      }) as [IUserData];
+      if (!userData || currentUser.role === 'user') {
+        container.append(this.alertView.createNotAdminAlert())
+      }
+      else {
+        container.append(this.sidebarView.create(userData));
+        const productsSection = this.createEl(`section`, ``, `admin-products`, container);
+        const productsEl = this.createEl(`div`, `Admin products`, `admin-products`, null);
+        this.createEl(`a`, `create new product`, `admin-products__name`, productsEl, `#/adminpanel/createproduct`);
+        const products = await this.api.getAllProduct();
+        console.dir(products);
+        productsSection.innerHTML = `
                     <a class="admin-products__create" href="#/adminpanel/createproduct">Create new product</a>
                     <ul class="admin-products__list">
                       ${this.getProductsHTML(products)}
                     </ul>
                     `;
-                productsSection.addEventListener(`click`, (e) => {
-                    const eventTarget = e.target as HTMLElement;
-                    const btn = eventTarget?.closest(`[data-delete-btn]`) as HTMLButtonElement;
-                    const item = eventTarget?.closest(`.admin-product`) as HTMLLIElement;
-                    if (btn && item && (typeof btn.dataset.id === `string`)) {
-                        this.api.removeProduct(userData, String(btn.dataset.id)).then(() => {
-                            item.style.opacity = `0`;
-                            function handleDelete(this: HTMLDivElement) {
-                                this.remove()
-                            }
-                            item.addEventListener(`transitionend`, handleDelete);
-                        })
-                    }
-                })
-            })();
-        }
-        return container;
-    }
+        productsSection.addEventListener(`click`, (e) => {
+          const eventTarget = e.target as HTMLElement;
+          const btn = eventTarget?.closest(`[data-delete-btn]`) as HTMLButtonElement;
+          const item = eventTarget?.closest(`.admin-product`) as HTMLLIElement;
+          if (btn && item && (typeof btn.dataset.id === `string`)) {
+            this.api.removeProduct(userData, String(btn.dataset.id)).then(() => {
+              item.style.opacity = `0`;
+              function handleDelete(this: HTMLDivElement) {
+                this.remove()
+              }
+              item.addEventListener(`transitionend`, handleDelete);
+            })
+          }
+        })
+      }
+    })();
 
-    getProductsHTML(products: IProduct[]) {
-        let res = ``
-        for (const product of products) {
-            res = `${res}
+    return container;
+  }
+
+  getProductsHTML(products: IProduct[]) {
+    let res = ``
+    for (const product of products) {
+      res = `${res}
                 <li class="admin-product">
                   <div class="admin-product__img-wrapper">
                     <img src="${product.image}" alt="Product photo">
@@ -92,24 +99,24 @@ class AdminProductsView extends Element {
                   </div>
                 </li>
             `;
-        }
-        return res;
     }
+    return res;
+  }
 
-    getQuantitiesHTML(quantities: string) {
-        const arr = quantities.split(`, `);
-        let res = ``;
-        for (const size of arr) {
-            let ending = ``;
-            if (+size.split(`:`)[1] > 1) {
-                ending = `s`;
-            }
-            res = `${res}
+  getQuantitiesHTML(quantities: string) {
+    const arr = quantities.split(`, `);
+    let res = ``;
+    for (const size of arr) {
+      let ending = ``;
+      if (+size.split(`:`)[1] > 1) {
+        ending = `s`;
+      }
+      res = `${res}
               <span>Size ${size.split(`:`)[0]}: ${size.split(`:`)[1]} piece${ending}</span>
               `;
-        }
-        return res;
     }
+    return res;
+  }
 }
 
 export default AdminProductsView;
